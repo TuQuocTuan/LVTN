3e9b56965866d2ec6bd85cd8d691274eee2e6d8c
TuQuocTuan <84710415+TuQuocTuan@users.noreply.github.com>
2026-06-06 16:05:23 +0700

    a

--- BE/src/controllers/ingredientController.js
+++ BE/src/controllers/ingredientController.js
@@ -15,3 +15,27 @@ export const getIngredients = async (req, res) => {
         res.status(500).json({ success: false, message: 'Lỗi không lấy được nguyên liệu' })
     }
 }
+
+export const addIngredients = async (req, res) => {
+    try {
+        const { name, quantity, unit, min_stock } = req.body;
+        if (!name || !quantity || !unit || !min_stock) {
+            return res.status(400).json({ success: false, message: 'Thieu thong tin nguyen lieu!' })
+        }
+        const { data, error } = await supabase
+            .from('ingredients')
+            .insert({
+                name,
+                quantity,
+                unit,
+                min_stock
+            })
+            .select()
+            .single();
+
+        if (error) throw error;
+        return res.status(201).json({ success: true, message: 'Thêm nguyên liệu thành công', data })
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error.message })
+    }
+}
