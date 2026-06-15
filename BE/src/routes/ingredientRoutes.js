5a3a221ff20a8769c6f9dbf9c1a5a7e0cb8f453d
TueTran <bigbang159vn@gmail.com>
2026-06-15 16:17:50 +0700

    púh

--- BE/src/routes/ingredientRoutes.js
+++ BE/src/routes/ingredientRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { getIngredients, addIngredients, updateIngredient, deleteIngredients, getIngredientsByCate } from '../controllers/ingredientController.js';
+import { getIngredients, addIngredients, updateIngredient, deleteIngredients } from '../controllers/ingredientController.js';
 
 const router = express.Router();
 
 router.get('/', getIngredients);
 router.post('/add', addIngredients);
 router.put('/update', updateIngredient);
-router.get('/:category_id', getIngredientsByCate);
 router.delete('/delete/:id', deleteIngredients);
 export default router;
