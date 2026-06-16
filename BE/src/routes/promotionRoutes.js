c00fa10f2765c3b7400250acec4137ba0e84ff8f
TuQuocTuan <tuan.tuquoc1502@gmail.com>
2026-06-16 17:53:17 +0700

    Lay voucher

--- BE/src/routes/promotionRoutes.js
+++ BE/src/routes/promotionRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllPromotions, addPromotions, updatePromotion, deletePromotion, getTotalBillCustomer, addVoucherCustomer } from '../controllers/promotionController.js';
+import { getAllPromotions, addPromotions, updatePromotion, deletePromotion, getTotalBillCustomer, addVoucherCustomer, getCustomerVoucher } from '../controllers/promotionController.js';
 
 const router = express.Router();
 
 router.get('/list', getAllPromotions);
+router.post('/customer-voucher', getCustomerVoucher);
 router.post('/add', addPromotions);
 router.post('/update', updatePromotion);
 router.post('/addvoucher', addVoucherCustomer);
