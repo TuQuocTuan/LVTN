83b1231c5950a6cc5466ede10a0135377da0b032
TuQuocTuan <84710415+TuQuocTuan@users.noreply.github.com>
2026-07-14 17:50:15 +0700

    a

--- FE/src/features/cashier/TableManager.jsx
+++ FE/src/features/cashier/TableManager.jsx
@@ -787,7 +787,7 @@ const TableManager = () => {
 
                                     // 1. Gọi API gửi lên Backend để chốt đóng phiên trong DB
                                     const response = await axios.post(`${API_BASE_URL}/checkout`, {
-                                      session_id: selectedTable?.current_session_id || selectedTable?.session_id || selectedTable?.id, // Hoặc biến chứa ID session hiện tại của bạn
+                                      session_id: localStorage.getItem('sessionId'),
                                       payment_method: 'VNPAY',// Bỏ qua check VNPAY tự động ở BE
                                       is_preview: false,
                                       close_user: userId, // ID user thực hiện đóng ca nếu có
