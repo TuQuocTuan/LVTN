e4f12957fcc6c926a54e2a5f35522aff43f24f94
TuQuocTuan <tuan.tuquoc1502@gmail.com>
2026-08-05 20:28:35 +0700

    a

--- BE/src/controllers/promotionController.js
+++ BE/src/controllers/promotionController.js
@@ -245,7 +245,7 @@ export const getTotalBillCustomer = async (req, res) => {
     }
 }
 
-// 🌟 API MỚI: Lấy danh sách lịch sử khách hàng đã nhận Voucher (Cho Tab "Danh sách Voucher đã tặng")
+//API MỚI: Lấy danh sách lịch sử khách hàng đã nhận Voucher (Cho Tab "Danh sách Voucher đã tặng")
 export const getCustomerVouchersHistory = async (req, res) => {
     try {
         const { data, error } = await supabase
